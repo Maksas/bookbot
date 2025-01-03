e35146afa065fe1d2d971bf1909f5beca767a998
Maksas <lukassimkus99@gmail.com>
2025-01-03 20:56:46 +0200

    Added a report about the books character composition

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
@@ -21,3 +23,23 @@ def count_letters(text):
             else:
                 dict[letter] += 1
     return(dict)
+
+def sort_on(dictonary):
+    return dictonary[1]
+
+
+def print_report():
+    dictonary = count_letters(main())
+    list_ = list(dictonary.items())
+    list_.sort(reverse=True, key=sort_on)
+    test = string.ascii_letters
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(main())} words found in the document")
+    print("")
+    for i in list_:
+        if i[0] in test:
+            print(f"The '{i[0]}' character was found {i[1]} times")
+    print("--- End report ---")
+    
+
+print_report()
