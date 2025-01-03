8e702aab475eb437bc88143d430d7556866c20e5
Maksas <lukassimkus99@gmail.com>
2025-01-03 19:17:12 +0200

    Added a function to count letters

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ def main():
         file_contents = f.read()
     return file_contents
 
-def count(text):
+def count_words(text):
     counter = 0
     words = text.split()
     for word in words:
         counter +=1
     return counter
 
-print(count(main()))
+def count_letters(text):
+    dict = {}
+    words = text.split()
+    for word in words:
+        word = word.lower()
+        for letter in word:
+            if letter not in dict:
+                dict[letter] = 1
+            else:
+                dict[letter] += 1
+    return(dict)
